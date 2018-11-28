1a2b235cf85529e395a840d5d57445450c6fc725
Madan Sapkota <code@madansapkota.com>
2018-11-28 15:54:59 +0545

    handle http request and process.

---
+++ src/app.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import headless from './headless';
+
+import '@babel/polyfill';
+
+// init express
+const app = express();
+
+// allow cross-origin requests
+app.use(cors({origin: true}));
+
+// request parser
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// handle cache request
+app.get('/cache', (req, res) => {
+    if (req.query.u) {
+        let testUrl = Buffer.from(req.query.u, 'base64').toString('ascii');
+        headless.render(testUrl).then((response) => {
+            res.send(response);
+        }).catch((e) => {
+            // handle the exception
+        });
+    } else {
+        res.send('Invalid Url and params!');
+    }
+});
+
+// handle all requests
+app.get('*', (req, res) => {
+    res.send('Cache server is Running!');
+});
+
+// start app in port 8095
+const port = process.env.PORT || 8095;
+app.listen(port, () => console.log('http://localhost:' + port));
