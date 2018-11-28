eeb15a66cfeb19ab7301e1af9435ed7b9eeebf19
Madan Sapkota <code@madansapkota.com>
2018-11-28 13:48:34 +0545

    eslint config for node.

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "node": true,
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off"
+    }
+};
