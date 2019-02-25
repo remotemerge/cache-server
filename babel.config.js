3575b29fb5d6aa102db9e98e13d3dc0fe1af784d
Madan Sapkota <code@madansapkota.com>
2019-02-25 07:07:59 +0000

    set node version for babel.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   presets: [
     [
-      '@babel/preset-env'
+      '@babel/preset-env',
+      {
+        targets: {
+          node: process.env.npm_package_node_version || 'current'
+        }
+      }
     ],
   ],
   ignore: ['dist/*', 'output/*', 'playground/*', 'public/*']
