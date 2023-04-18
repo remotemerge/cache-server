cd725890a02320feeb9ba5c80d3c7341c60d8eb5
Madan Sapkota <contact@madansapkota.com>
2023-04-18 08:12:13 +0545

    Use ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import tsEslintParser from '@typescript-eslint/parser';
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin';
+import jsdocEslintPlugin from 'eslint-plugin-jsdoc';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**', '**/*.html'],
+  },
+  {
+    files: ['**/*.ts', '**/*.js'],
+    rules: {
+      'linebreak-style': ['error', 'unix'],
+      'no-console': 'warn',
+      'no-undef': 'error',
+      'no-unused-vars': 'error',
+      'prefer-const': 'error',
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+    },
+    plugins: {
+      jsdocEslintPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        process: 'readonly',
+      }
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsEslintParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsEslintPlugin,
+    },
+  },
+];
