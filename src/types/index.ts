1e068226375aa72d812d485b7b89c52da703c142
Madan Sapkota <contact@madansapkota.com>
2023-04-18 11:48:17 +0545

    Add types

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type HeadlessConfig = {
+  url: string;
+  userAgent: string;
+  wait: number;
+  headless: boolean;
+};
+
+export type CliConfig = {
+  host: string;
+  port: number;
+  wait: number;
+  headless: boolean;
+};
